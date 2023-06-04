509b4d9b13316a7abdf9304f1887a19265d2b20c
igorbratus <brat.igor1992@gmail.com>
2023-06-04 06:55:55 -0400

    unit commit

--- src/Main.java
+++ src/Main.java
@@ -11,10 +11,11 @@ public class Main {
     }
 
     public static void task1() {
+        int money = 2_459_000;
         int total = 0;
         int month = 0;
         int depositMonth = 15_000;
-        while (total < 2_459_000) {
+        while (total < money) {
             System.out.println(month + " месяцев понадобиться что бы накопить " + total);
             total = total + depositMonth;
             month = month + 1;
@@ -49,9 +50,10 @@ public class Main {
         int month = 0;
         double total = 15000.0;
         double limit = 12000000.0;
+        double percent = 7.0;
         while (total < limit) {
             month++;
-            total = total * 1.07;
+            total *= total * percent / 100;
             System.out.printf("месяц %d: сумма  накоплений составляет %.2f рублей %n", month, total);
             System.out.println();
         }
@@ -65,7 +67,7 @@ public class Main {
         double percent = 7.0;
         while (total < limit) {
             month++;
-            total = total * total * percent / 100;
+            total += total * percent / 100;
             if (month % 6 == 0) {
                 System.out.printf("месяц %d: сумма  накоплений составляет %.2f рублей %n", month, total);
             }
@@ -75,38 +77,31 @@ public class Main {
     public static void task6() {
         int month = 0;
         double total = 15000.0;
-        int years9 = 9 * 12;
+        int years = 9 * 12;
         double percent = 7.0;
-        while (month < years9) {
+        while (month < years) {
             month++;
-            total = total + total * percent / 100;
+            total += total * percent / 100;
             if (month % 6 == 0) {
                 System.out.printf("месяц %d: сумма  накоплений составляет %.2f рублей %n", month, total);
             }
         }
-//        int month;
-//        double total = 15000.0;
-//        int years9 = 9 * 12;
-//        double percent = 7.0;
-//        for (month = 0; month <= years9; month++) {
-//            total = total + total * percent / 100;
-//            if (month % 6 == 0) {
-//                System.out.printf("месяц %d: сумма  накоплений составляет %.2f рублей %n", month, total);
-//            }
     }
 
 
     public static void task7() {
         int daysInMonth = 31;
-        for (int friday = 5; friday <= daysInMonth; friday = friday + 7) {
+        int friday = 5;
+        for (; friday <= daysInMonth; friday += 7) {
             System.out.println(friday + " число, пятница отчетный день напоминание! Необходимо подготовить отчет");
         }
     }
 
     public static void task8() {
+        int years = 2023;
         int cometEvery79Year = 79;
-        int after2023Years = 2023 + 100;
-        int before2023years = 2023 - 200;
+        int after2023Years = years + 100;
+        int before2023years = years - 200;
         for (int comet = 0; comet < after2023Years; comet = comet + cometEvery79Year) {
             if (comet > before2023years) {
                 System.out.println(comet);
